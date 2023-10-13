1b25acac11d0d9919f1c0d649fe90f2a90698356
kennethn23 <kennethng1337@gmail.com>
2023-10-13 09:19:29 -0400

    10/13/23

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const DOMSelectors = {
-    button: document.getElementById("button"), // getElementById looks for the id that is passed through it, in this case being "btn"
-    text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
-    box: document.getElementById("big-black-box"),
-    points: document.querySelectorAll(".point"), // querySelectorAll will access multiple elements with the same class, in this case being "point"
-    null: document.getElementById("point"), // if you try to access a class with getElementById, it will result in "null"
-    empty: document.querySelector(".nothing"),
-  };
-  
-  console.log(DOMSelectors.box);
-  console.log(DOMSelectors.button);
-  console.log(DOMSelectors.text);
-  console.log(DOMSelectors.points); 
-  console.log(DOMSelectors.points[0]); // this returns the first ever element in the document with class 'point'
-  console.log(DOMSelectors.null);
-  console.log(DOMSelectors.nothing);
+  form: document.querySelector("#form"),
+  firstName: document.querySelector(".first-name"),
+  h2: document.querySelectorAll("h2"),
+}
+
+DOMSelectors.form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log(DOMSelectors.firstName.value);
+  DOMSelectors.h2.forEach((element) => element.textContent = DOMSelectors.firstName.value);
+});
