1fa3e03d63702b77b58d207825de5d07baa1d710
kennethn23 <kennethng1337@gmail.com>
2023-10-16 09:21:14 -0400

    10/16/23

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const DOMSelectors = {
   form: document.querySelector("#form"),
   firstName: document.querySelector(".first-name"),
   h2: document.querySelectorAll("h2"),
+  box: document.querySelector("#container-box"),
 }
 
-DOMSelectors.form.addEventListener("submit", function(event) {
+/* DOMSelectors.form.addEventListener("submit", function(event) {
   event.preventDefault();
   console.log(DOMSelectors.firstName.value);
   DOMSelectors.h2.forEach((element) => element.textContent = DOMSelectors.firstName.value);
+}); */
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let input = DOMSelectors.firstName.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
+  DOMSelectors.firstName.value = "";
 });
