c641bc8a06d1dea730bc1ced8ec4d8f95fea87e1
kennethn23 <kennethng1337@gmail.com>
2023-10-17 09:23:47 -0400

    10/17/23

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const DOMSelectors = {
   form: document.querySelector("#form"),
   firstName: document.querySelector(".first-name"),
-  h2: document.querySelectorAll("h2"),
+  lastName: document.querySelector(".last-name"),
+  height: document.querySelector(".height"),
+  // h2: document.querySelectorAll("h2"),
   box: document.querySelector("#container-box"),
 }
 
@@ -11,9 +13,33 @@ const DOMSelectors = {
   DOMSelectors.h2.forEach((element) => element.textContent = DOMSelectors.firstName.value);
 }); */
 
+function Enter () {
+  DOMSelectors.box.insertAdjacentHTML("beforeend", 
+  `<div class="entry">
+    <h3>${DOMSelectors.firstName.value} ${DOMSelectors.lastName.value}</h3>
+    <p>Height: ${DOMSelectors.height.value}</p>
+    <button class="remove">Remove</button>
+  </div>`);
+}
+
+function Remove () {
+  const removeButtons = document.querySelectorAll(".remove");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", function (button) {
+      button.target.parentElement.remove();
+    })
+  });
+}
+
+function Clear () {
+  DOMSelectors.firstName.value = "";
+  DOMSelectors.lastName.value = "";
+  DOMSelectors.height.value = "";
+}
+
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  let input = DOMSelectors.firstName.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
-  DOMSelectors.firstName.value = "";
+  Enter();
+  Remove();
+  Clear();
 });
